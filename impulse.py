bae9715190812046d7b24ad0aa3df562ac0a7078
Vibius Vibidius Zosimus <mudsharkmk@gmail.com>
2016-12-05 22:50:04 -0500

    try data

--- impulse.py
+++ impulse.py
@@ -2,13 +2,13 @@ from visual import *
 from visual.graph import *
 from sys import argv
 
-try:
-    script, filename = argv
+# try:
+script, filename = argv
 
-    target = open(filename, 'w')
-    target.truncate()
-except:
-    pass
+target = open(filename, 'w')
+target.truncate()
+# except:
+#     pass
 
 
 def swing(angle1, angle2):
@@ -21,8 +21,8 @@ def swing(angle1, angle2):
     top_mass.pos = vector(10, 0, 0).rotate(angle = angle1, axis = (0, 0, 1))
     bot_mass.pos = top_mass.pos + vector(10, 0, 0).rotate(angle = angle2, axis = (0, 0, 1))
 
-    tip_top = helix(const = 50, len = 10, pos = fixed_tip.pos, radius = .5, thickness = .1, coils = 10, color = color.yellow)
-    top_bot = helix(const = 50, len = 10, radius = .5, thickness = .1, coils = 10, color = color.cyan)
+    tip_top = helix(const = 9999, len = 10, pos = fixed_tip.pos, radius = .5, thickness = .1, coils = 10, color = color.yellow)
+    top_bot = helix(const = 9999, len = 10, radius = .5, thickness = .1, coils = 10, color = color.cyan)
 
     tip_top.axis = top_mass.pos - fixed_tip.pos
 
@@ -82,8 +82,8 @@ def swing(angle1, angle2):
     dangle_bot = angle_bot - angle_bot_old
 
     # while not flipped and timer < 500:
-    while timer < 500:
-        # rate(10)
+    while timer < 10000:
+        # rate(200)
         # top_mass.trail.append(pos = top_mass.pos)
         # bot_mass.trail.append(pos = bot_mass.pos)
 
@@ -97,17 +97,17 @@ def swing(angle1, angle2):
 
         tip_top.axis = top_mass.pos - fixed_tip.pos
 
-        angle_top_old = angle_top
-
-        if tip_top.axis.y < 0:
-                 angle_top = atan(-tip_top.axis.x / tip_top.axis.y)
-        else:
-            if tip_top.axis.x >= 0:
-                angle_top = pi / 2 + atan(tip_top.axis.y / tip_top.axis.x)
-            else:
-                tip_top.axis.x < 0
-
-        dangle_top = angle_top - angle_top_old
+        # angle_top_old = angle_top
+        #
+        # if tip_top.axis.y < 0:
+        #          angle_top = atan(-tip_top.axis.x / tip_top.axis.y)
+        # else:
+        #     if tip_top.axis.x >= 0:
+        #         angle_top = pi / 2 + atan(tip_top.axis.y / tip_top.axis.x)
+        #     else:
+        #         tip_top.axis.x < 0
+        #
+        # dangle_top = angle_top - angle_top_old
 
         was_left = False
         was_right = False
@@ -142,17 +142,17 @@ def swing(angle1, angle2):
         else:
             flipped = False
 
-        angle_bot_old = angle_bot
-
-        if top_bot.axis.y < 0:
-            angle_bot = atan(-top_bot.axis.x / top_bot.axis.y)
-        else:
-            if top_bot.axis.x >= 0:
-                angle_bot = pi / 2 + atan(top_bot.axis.y / top_bot.axis.x)
-            else:
-                top_bot.axis.x < 0
-
-        dangle_bot = angle_bot - angle_bot_old
+        # angle_bot_old = angle_bot
+        #
+        # if top_bot.axis.y < 0:
+        #     angle_bot = atan(-top_bot.axis.x / top_bot.axis.y)
+        # else:
+        #     if top_bot.axis.x >= 0:
+        #         angle_bot = pi / 2 + atan(top_bot.axis.y / top_bot.axis.x)
+        #     else:
+        #         top_bot.axis.x < 0
+        #
+        # dangle_bot = angle_bot - angle_bot_old
 
         top_mass.Fs_up = -tip_top.const * (mag(tip_top.axis) - tip_top.len) * norm(tip_top.axis)
 
@@ -166,13 +166,15 @@ def swing(angle1, angle2):
 
         timer += dt
 
-        try:
-            target.write(str(dangle_bot) + ", ")
-        except:
-            pass
+    target.write(str(timer))
 
-theta1_first = 90
-theta1_last = 95
+    # try:
+    #     target.write(str(timer))
+    # except:
+    #     pass
+
+theta1_first = 120
+theta1_last = 125
 
 theta2_first = 90
 theta2_last = 95
@@ -184,20 +186,20 @@ for theta1 in theta1_range:
     for theta2 in theta2_range:
         print "%r, %r" % (theta1, theta2)
         swing(-radians(90 - theta1), -radians(90 - theta2))
-    #     if theta2 != theta2_range[-1]:
-    #         try:
-    #             target.write(", ")
-    #         except:
-    #             pass
+        if theta2 != theta2_range[-1]:
+            # try:
+            target.write(", ")
+            # except:
+            #     pass
     # try:
-    #     target.write("\n")
+    target.write("\n")
     # except:
     #     pass
 
-try:
-    target.close()
-    print "closed."
-except:
-    pass
+# try:
+target.close()
+#     print "closed."
+# except:
+#     pass
 
 print "done."
